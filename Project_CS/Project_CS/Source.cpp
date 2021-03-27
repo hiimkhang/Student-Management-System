c2916b2c36d29ab7898243327a2229f74949d5db
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2021-03-27 12:30:22 +0700

    update header

--- Project_CS/Project_CS/Source.cpp
+++ Project_CS/Project_CS/Source.cpp
@@ -1,9 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "hello";
-	cout << "Khang's first commit.\n";
-	cout << "son coomiit abvcsss";
-	return 0;
-}
+#include "Header.h"
