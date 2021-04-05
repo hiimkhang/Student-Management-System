a462a9104c94781de63f5f63dffbb45039198b82
khangdeptraii02 <phanvinhkhang910@gmail.com>
2021-04-05 17:14:38 +0700

    update solution

--- Project_CS/Project_CS/Header.h
+++ Project_CS/Project_CS/Header.h
@@ -21,7 +21,7 @@ struct Course {
     string teacherName;
     int creditNum;
     int maxNum_ofStudents = 50;
-    string courseDate[6] = { "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    string courseDate[6] = { "MON", "TUE", "WED", "THU", "FRI", "SAT" };
     string courseSession[4] = { "07:30", "09:30", "13:30", "15:30" };
     Course* pNext;
 };
@@ -36,13 +36,11 @@ struct Class {
 struct Student {
     int No, StudentID, SocialID;
     string studentPassword = "12345678";
-    string Firstname, Lastname; 
+    string Firstname, Lastname;
     string Gender;
     string DoB;
 };
 
-<<<<<<< HEAD
-=======
 struct Staff {
     string nameStaff;
     string staffAccount, staffPassword = "staff";
@@ -52,5 +50,4 @@ struct Staff {
 void loginStaff(Staff*& staff);
 
 
->>>>>>> 3decbec48ede187a5afb4aff7352b78c908dc0e7
 #endif
