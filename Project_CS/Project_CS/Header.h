4c89b2e7f3891d362a7dd8d66394cc54093f517b
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2021-04-05 12:00:26 +0700

    update main

--- Project_CS/Project_CS/Header.h
+++ Project_CS/Project_CS/Header.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <iostream>
+#include <fstream>
+#include "string.h"
+using namespace std;
+
+struct StudentInfo {
+    int No, StudentID, SocialID;
+    char *Firstname, *Lastname; 
+    char Gender[6];
+    string DoB;
+};
+
+// Function to input First name and Last name of student.
+void inputName (char *name);
+// Function to input student's information using handwork.
+void inputInfo (StudentInfo &SI);
+// Function to input student's information using CSV file.
+void loadInfo (StudentInfo &SI, char *filename);
+#endif
---
+++ Project_CS/Project_CS/main.cpp
@@ -0,0 +1,87 @@
+#include "Header.h"
+
+int main() {
+	// load data
+	char choose;
+	string str;
+	do {
+		cout << "a. Staff \n";
+		cout << "b. Student \n";
+		cout << "c. Exit \n";
+		cout << "Please input: ";
+		cin >> choose;
+		if (choose == 'c') {
+			cout << "See you later! \n";
+			break;
+		}
+		else if (choose == 'a') {
+			do {
+				cout << "a. School year \n";
+				cout << "b. View profile \n";
+				cout << "c. Change password \n";
+				cout << "d. Log out \n";
+				cout << "Please input: ";
+				cin >> choose;
+				if (choose == 'd') break;
+				else if (choose == 'a') {
+					do {
+						//cout << danh sach cac nam;
+						cout << "a. Create new year \n";
+						cout << "b. Exit \n";
+						cout << "Please input the year (or press a/b): ";
+						cin >> str;
+						if (str == "b") break;
+						else if (str == "a") {
+							do {
+								cout << "a. Semester \n";
+								cout << "b. Student \n";
+								cout << "c. Exit \n";
+								cout << "Please input: ";
+								cin >> choose;
+								if (choose == 'c') break;
+								else if (choose == 'a') {
+
+								}
+								else if (choose == 'b') {
+
+								}
+								else {
+									cout << "Please input a or b or c! \n";
+									cout << "Please input: ";
+									cin >> choose;
+								}
+							} while (choose != 'a' && choose != 'b' && choose != 'c');
+						}
+						else if (str == "year.name") {
+
+						}
+						else {
+							cout << "Error input \n";
+							cout << "Please input again: ";
+							cin >> str;
+						}
+					} while (str != "a" && str != "b" && nam ko co trong list);
+				}
+				else if (choose == 'b') {
+
+				}
+				else if (choose == 'c') {
+
+				}
+				else {
+					cout << "Please input a or b or c or d! \n";
+					cout << "Please input: ";
+					cin >> choose;
+				}
+			} while (choose != 'a' && choose != 'b' && choose != 'c' && choose != 'd');
+		}
+		else if (choose == 'b') {
+
+		}
+		else {
+			cout << "Please input a or b or c! \n";
+			cout << "Please input: ";
+			cin >> choose;
+		}
+	} while (choose != 'a' && choose != 'b' && choose != 'c');
+}
