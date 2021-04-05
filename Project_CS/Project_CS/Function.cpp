1a263dd7ddea2817223641bab6282d9e9aa09e92
minh-tuan-184 <81298170+minh-tuan-184@users.noreply.github.com>
2021-04-05 17:35:42 +0700

    update

--- Project_CS/Project_CS/Function.cpp
+++ Project_CS/Project_CS/Function.cpp
@@ -1,1 +1,49 @@
 #include "Header.h"
+
+
+	void getData(Staff * &pHead, string filename) {
+	ifstream in;
+	in.open(filename);
+	if (in) {
+		string nameStaff, staffAccount, staffPassword = "staff";
+		char tmp;
+		Staff* pCur = pHead;
+		while (!in.eof()) {
+			if (pHead == nullptr) {
+				pHead = new Staff;
+				pCur = pHead;
+			}
+			else {
+				pCur->pNext = new Staff;
+				pCur = pCur->pNext;
+			}
+			getline(in, nameStaff, '_');
+			getline(in, staffAccount, '_');
+			getline(in, staffPassword, '_');
+			pCur->nameStaff = nameStaff;
+			pCur->staffAccount = staffAccount;
+			pCur->staffPassword = staffPassword;
+			pCur->pNext = nullptr;
+		}
+		in.close();
+	}
+	else cout << "ERROR \n";
+}
+void display(Staff* pHead) {
+	while (pHead) {
+		cout << "Name: " << pHead->nameStaff << endl;
+		cout << "Account" << pHead->staffAccount << endl;
+		cout << "Password" << pHead->staffPassword << endl;
+
+	}
+}
+void deleteList(Staff*& pHead) {
+	Staff *pCur = pHead;
+	while (pHead) {
+		pHead = pHead->pNext;
+		delete pCur;
+		pCur = pHead;
+	}
+}
+
+
--- Project_CS/Project_CS/Header.h
+++ Project_CS/Project_CS/Header.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <fstream>
-#include "string.h"
+#include <string>
 using namespace std;
 
 struct YearSchool {
